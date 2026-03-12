ebc604e1a084cb12c1ab3de5e8000294215b727a
bari <jabarilucien@gmail.com>
2026-03-12 03:10:21 -0400

    partial

--- src/nsm_subdomain_scanner.py
+++ src/nsm_subdomain_scanner.py
@@ -190,12 +190,12 @@ class Subdomain_Scanner():
                 if domains:
                     for domain in domains:
                         for sub in wordlist:
-                            while len(futures) < max_threads:
+                            while len(futures) >= max_threads:
+                                futures = [f for f in futures if not f.done()]
 
-                                futures.append(executor.submit(Subdomain_Scanner._subdomain_scanner, domain, sub))
+                            futures.append(executor.submit(Subdomain_Scanner._subdomain_scanner, domain, sub))
 
-                            Variables.panel.renderable = (f"Target:[{c5}] {sub}.{domain}[/{c5}]  -  Max_Workers:[{c5}] {Variables.max_threads}[/{c5}]  -  Wordlist:[{c5}] {Variables.s_name}[/{c5}]  -  Errors:[{c5}] {Variables.errors}[/{c5}]")
-                            futures = [f for f in futures if not f.done()]                  
+                            Variables.panel.renderable = (f"Target:[{c5}] {sub}.{domain}[/{c5}]  -  Max_Workers:[{c5}] {Variables.max_threads}[/{c5}]  -  Wordlist:[{c5}] {Variables.s_name}[/{c5}]  -  Errors:[{c5}] {Variables.errors}[/{c5}]")                  
 
 
                 elif url:
